4835762ab8a3f8bc59114b1053ef877b0ee9ae40
vagrant <vagrant@localhost.localdomain>
2020-07-04 05:20:34 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  get 'favorites/create'
+  get 'favorites/destroy'
+  root 'homes#top' 
+  get 'home/about' => 'homes#about'
+
+  devise_for :users
+
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+
+end
